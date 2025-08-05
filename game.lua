4d2e7170ca9dcd3e3b6187df38dfaa10643d0f6f
based <based@arch>
2025-08-05 12:42:08 -0400

    idk its been a while

--- game.lua
+++ game.lua
@@ -1,7 +1,3 @@
 function process_message(msg)
-	print('greetings:\n')
-	for i, v in ipairs (msg.hello) do
-		print('\thello ' .. v .. '(' .. i .. ')\n')
-	end
-	print('oh yeah, one more thing: ' .. msg.another)
+	print('i got ', msg)
 end
